b9e16b28dac6bd5a80f7fea3fc5d43a068411475
ihar.karavatski <ihar.karavatski@itchart-group.com>
2018-02-06 18:07:59 +0300

    Start chapter 5.

---
+++ chapter5/blocks.rb
@@ -0,0 +1,38 @@
+def my_method(&my_block)
+  puts "We're in the method, about to invoke your block!"
+  my_block.call
+  puts "We're back in the method!"
+end
+
+my_method do
+  puts "We're in the block!"
+end
+
+my_method do
+  puts "It's a block party!"
+end
+
+my_method do
+  puts "Wooooo!"
+end
+
+def twice(&my_block)
+  puts "In the method, about to call the block!"
+  my_block.call
+  puts "Back in the method, about to call the block again!"
+  my_block.call
+  puts "Back in the method, about to return!"
+end
+
+twice do
+  puts "Woooo!"
+end
+
+def give(&my_block)
+  my_block.call("2 turtle doves", "1 partridge")
+end
+
+give do |present1, present2|
+  puts "My method gave to me ..."
+  puts present1, present2
+end
