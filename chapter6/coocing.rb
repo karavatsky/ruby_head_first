4a82fa2d69a38f0d345b221d556e7e2ace402786
ihar.karavatski <ihar.karavatski@itchart-group.com>
2018-02-12 18:18:40 +0300

    Chapter 7 continue.

---
+++ chapter6/coocing.rb
@@ -0,0 +1,16 @@
+
+def make_casserole
+  puts "Preheat oven to 375 degrees"
+  ingredients = yield
+  puts "Place #{ingredients} in dish"
+  puts "Bake for 20 minutes"
+  ingredients = "rice, broccoli, and chicken"
+end
+
+make_casserole do
+  "rice, broccoli, and chicken"
+end
+
+make_casserole do
+  "noodles, celery, and tuna"
+end
--- chapter6/film_adjectives.rb
+++ chapter6/film_adjectives.rb
@@ -1,6 +1,24 @@
-review_file = File.open("reviews.txt")
-puts review_file.class
-lines = review_file.readlines
-puts "Line 4: #{lines[3]}"
-puts "Line 1: #{lines[0]}"
-review_file.close
+lines = []
+
+File.open("reviews.txt") do |review_file|
+  lines = review_file.readlines()
+end
+
+relevant_lines = lines.find_all { |line| line.include?("Truncated") }
+reviews = relevant_lines.reject { |line| line.include?("--") }
+puts relevant_lines
+puts
+puts reviews
+puts
+def find_adjective(string)
+  words = string.split(" ")
+  index = words.find_index("is")
+  words[index + 1]
+end
+
+adjectives = []
+reviews.each do |review|
+  adjectives << find_adjective(review)
+end
+
+puts adjectives
