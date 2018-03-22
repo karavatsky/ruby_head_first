6efb81679829c83f8282d7ee43aade15f954cddf
ihar.karavatski <ihar.karavatski@itchart-group.com>
2018-03-22 18:00:00 +0300

    Compleate chapter 7.

--- chapter7/votes.rb
+++ chapter7/votes.rb
@@ -1,3 +1,16 @@
+class Candidate
+  attr_accessor :name, :age, :occupation, :hobby,  :birthplace
+  def initialize(name, age:, occupation:, hobby: nil, birthplace: "Sleepy Creek")
+    self.name = name
+    self.age = age
+    self.occupation = occupation
+    self.hobby = hobby
+    self.birthplace = birthplace
+  end
+end
+
+p Candidate.new("Carl Barnes", age: 49, occupation: "Attorney")
+
 lines = []
 File.open("votes.txt") do |file|
   lines= file.readlines
